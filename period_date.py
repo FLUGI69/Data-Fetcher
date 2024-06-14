7b65b914231cd5480d3afab68cf7b68026175954
FLUGI69 <167913031+FLUGI69@users.noreply.github.com>
2024-06-15 00:37:06 +0200

    Create period_date.py

---
+++ period_date.py
@@ -0,0 +1,15 @@
+class Date:
+    def __init__(self,
+                ticker: str,
+                start: str,
+                end: str, 
+                interval: str
+                ):
+        
+        self.ticker = ticker
+
+        self.start = start
+
+        self.end = end
+
+        self.interval = interval
